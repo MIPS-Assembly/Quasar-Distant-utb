d67968ae5004fba7230897b723343e2d5e44329e
hao3004 <34440957+hao3004@users.noreply.github.com>
2018-03-31 13:31:13 +0700

    Update printing.asm

--- printing.asm
+++ printing.asm
@@ -22,3 +22,24 @@
         lw $a0, age
         syscall
 # **********************************************
+.data
+        PI: .float 3.14
+                # this is a word or interger
+.text
+        li $v0, 2 
+                # print anh interger to the screen
+        lwc1 $f12, PI 
+                # lwc1 = load co-processor 1
+        syscall
+# **********************************************
+.data
+        myDouble: .double 7.202
+        zeroDouble: .double 0.0
+.text
+        ldc1 $f2, myDoule
+        ldc1 $f0, zeroDouble
+        
+        li      $v0, 3
+        add.d   $f12, $f2, $f0
+        syscall
+# **********************************************
